658577ae004d9cd58b2a442ce4db724598d5ccc8
Hans Peter <matija.jajcinovic@tum.de>
2023-06-10 02:29:16 +0200

    added custom RayCastCnn

--- Assets/ML-Agents/RayCast2DCNN/OptimizedRayCast.cs
+++ Assets/ML-Agents/RayCast2DCNN/OptimizedRayCast.cs
@@ -1,61 +1,89 @@
 using Unity.Collections;
 using Unity.Jobs;
 using UnityEngine;
+using Unity.MLAgents.Sensors;
+using Unity.Mathematics;
 
 public class OptimizedRayCast : MonoBehaviour
 {
     public Vector3 focalPoint = Vector3.zero;
-    public Transform relativeTo;
-    public float rayDistance = 5f;
+    public Transform root;
+    public float rayMaxDistance = 5f;
     //public int numberOfRays = 1000;
     NativeArray<RaycastHit> results;
     NativeArray<RaycastCommand> commands;
     public Mesh mesh;
+    public Texture2D texture2D;
+    public int width;
+    public int height;
+
+    [HideInInspector]
+    public static int id = 0;
+    public int stack = 1;
+
+    private void Awake()
+    {
+        texture2D = new Texture2D(width, height, TextureFormat.RGB24, false);
+        string s = "RayCNNSensor" + id;
+        id++;
+        //init sensor
+        RayCNNComponent r = gameObject.AddComponent<RayCNNComponent>();
+        r.optimizedRayCast = this;
+        r.Grayscale = false;
+        //r.RenderTexture = render;
+        r.texture2D = texture2D;
+        r.CompressionType = SensorCompressionType.PNG;
+        r.SensorName = s;
+        r.ObservationStacks = stack;
+
+
+
+    }
     void CleanMesh()
     {
 
-            mesh = GetComponent<MeshFilter>().mesh;
-            Vector3[] vertices = mesh.vertices;
-            int[] triangles = mesh.triangles;
+        mesh = GetComponent<MeshFilter>().mesh;
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
 
-            // Use a dictionary to remove duplicates.
-            System.Collections.Generic.Dictionary<Vector3, int> vertexDictionary = new System.Collections.Generic.Dictionary<Vector3, int>();
+        // Use a dictionary to remove duplicates.
+        System.Collections.Generic.Dictionary<Vector3, int> vertexDictionary = new System.Collections.Generic.Dictionary<Vector3, int>();
 
-            for (int i = 0; i < vertices.Length; i++)
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            if (!vertexDictionary.ContainsKey(vertices[i]))
             {
-                if (!vertexDictionary.ContainsKey(vertices[i]))
-                {
-                    vertexDictionary.Add(vertices[i], vertexDictionary.Count);
-                }
+                vertexDictionary.Add(vertices[i], vertexDictionary.Count);
             }
+        }
 
-            // Reassign the vertices and triangles.
-            Vector3[] newVertices = new Vector3[vertexDictionary.Count];
-            int[] newTriangles = new int[triangles.Length];
+        // Reassign the vertices and triangles.
+        Vector3[] newVertices = new Vector3[vertexDictionary.Count];
+        int[] newTriangles = new int[triangles.Length];
 
-            vertexDictionary.Keys.CopyTo(newVertices, 0);
+        vertexDictionary.Keys.CopyTo(newVertices, 0);
 
-            for (int i = 0; i < triangles.Length; i++)
-            {
-                newTriangles[i] = vertexDictionary[vertices[triangles[i]]];
-            }
+        for (int i = 0; i < triangles.Length; i++)
+        {
+            newTriangles[i] = vertexDictionary[vertices[triangles[i]]];
+        }
 
-            mesh.Clear();
+        mesh.Clear();
 
-            mesh.vertices = newVertices;
-            mesh.triangles = newTriangles;
+        mesh.vertices = newVertices;
+        mesh.triangles = newTriangles;
 
-            mesh.RecalculateNormals();
-            mesh.RecalculateBounds();
-        
-    
-}
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+
+
+    }
     int numberOfRays = 0;
     private void Start()
     {
-        if (!relativeTo)
+        if (!root)
         {
-            relativeTo = transform;
+            root = transform;
         }
         numberOfRays = mesh.vertexCount;
         results = new NativeArray<RaycastHit>(numberOfRays, Allocator.Persistent);
@@ -63,23 +91,61 @@ public class OptimizedRayCast : MonoBehaviour
     }
     private void FixedUpdate()
     {
-        CastMultiThreadRays();
+        CastMainThreadRays();
     }
+    public void SaveInTexture(float3 v, Vector2 uv)
+    {
+        v = (v + 1) / 2.0f;
+        v = math.clamp(v,0,1);
+        Color c = new Color(v.x,v.y,v.z);
+        
+        texture2D.SetPixel((int)(uv.x * texture2D.width),(int)(uv.y*texture2D.height),c); 
+    }
+    Color[] colors;
     private void CastMainThreadRays()
     {
+        ResetPixels();
+        Vector3[] vertices = mesh.vertices;
+        Vector2[] uvs = mesh.uv;
+        //colors = texture2D.GetPixels();
         for (int i = 0; i < numberOfRays; i++)
         {
-
-            Vector3 direction = new Vector3(0, 0, 0);
+            
+            Vector3 direction = (transform.TransformPoint(vertices[i]) - root.position).normalized;
             Ray ray = new Ray(transform.position, direction);
-            Debug.DrawRay(ray.origin, ray.direction * rayDistance, Color.red);
+            Color debugC = Color.green;
+            
 
             RaycastHit hit;
-            if (Physics.Raycast(ray, out hit, rayDistance))
+            if (Physics.Raycast(ray, out hit, rayMaxDistance))
             {
-                Debug.Log("Hit object: " + hit.collider.gameObject.name);
+                Vector3 v = (hit.distance * ray.direction) / rayMaxDistance;
+                v = root.InverseTransformDirection(v);
+                Debug.Log(v);
+                SaveInTexture(v, uvs[i]);
+                debugC = Color.red;
+                Debug.DrawRay(ray.origin, ray.direction * hit.distance, debugC);
             }
+            else
+            {
+                Debug.DrawRay(ray.origin, ray.direction * rayMaxDistance, debugC);
+            }
+            
         }
+        texture2D.Apply();
+    }
+    public void ResetPixels()
+    {
+        Color[] colors = this.texture2D.GetPixels();
+        int ii = 0;
+        foreach (var x in colors)
+        {
+            colors[ii] = new Color(0, 0, 0);
+            ii++;
+        }
+        texture2D.SetPixels(colors);
+        texture2D.Apply();
+        
     }
     private void CastMultiThreadRays()
     {
@@ -87,28 +153,21 @@ public class OptimizedRayCast : MonoBehaviour
         Vector3[] vertices = mesh.vertices;
         for (int i = 0; i < numberOfRays; i++)
         {
-            Vector3 direction;
-            if (transform != relativeTo)
-            {
-                direction = relativeTo.InverseTransformPoint(transform.TransformPoint(vertices[i])) - focalPoint;
-            }
-            else
-            {
-                direction = (transform.TransformPoint(vertices[i])) - focalPoint;
-            }
-           
-            commands[i] = new RaycastCommand(transform.position, direction, rayDistance);
+            Vector3 direction = vertices[i].normalized;
+            
+
+            commands[i] = new RaycastCommand(transform.position, direction, rayMaxDistance);
         }
 
         JobHandle handle = RaycastCommand.ScheduleBatch(commands, results, 1, default(JobHandle));
         handle.Complete();
 
-        for (int i = 0; i < numberOfRays; i++)
+        for (int i = 0; i < results.Length; i++)
         {
             if (results[i].collider != null)
             {
-                Debug.Log("Hit object: " + results[i].collider.gameObject.name);
-                Debug.DrawRay(transform.position, commands[i].direction * rayDistance, Color.red);
+               
+                Debug.DrawRay(transform.position, commands[i].direction * results[i].distance, Color.red);
             }
         }
     }
---
+++ Assets/ML-Agents/RayCast2DCNN/RayCNNComponent.cs
@@ -0,0 +1,259 @@
+using System;
+using UnityEngine;
+
+namespace Unity.MLAgents.Sensors
+{
+    /// <summary>
+    /// Sensor class that wraps a [RenderTexture](https://docs.unity3d.com/ScriptReference/RenderTexture.html) instance.
+    /// </summary>
+    /// 
+
+    using System;
+    using UnityEngine;
+    using UnityEngine.Serialization;
+
+    /// <summary>
+    /// Component that wraps a <see cref="RenderTextureSensor"/>.
+    /// </summary>
+
+    public class RayCNNComponent : SensorComponent, IDisposable
+    {
+        RayCNNSensor m_Sensor;
+
+        /// <summary>
+        /// The [RenderTexture](https://docs.unity3d.com/ScriptReference/RenderTexture.html) instance
+        /// that the associated <see cref="RenderTextureSensor"/> wraps.
+        /// </summary>
+        [HideInInspector, SerializeField, FormerlySerializedAs("renderTexture")]
+        Texture2D m_texture2D;
+
+        /// <summary>
+        /// Stores the [RenderTexture](https://docs.unity3d.com/ScriptReference/RenderTexture.html)
+        /// associated with this sensor.
+        /// </summary>
+        public Texture2D texture2D
+        {
+            get { return m_texture2D; }
+            set { m_texture2D = value; }
+        }
+
+        [HideInInspector, SerializeField, FormerlySerializedAs("sensorName")]
+        string m_SensorName = "RenderTextureSensor";
+
+        /// <summary>
+        /// Name of the generated <see cref="RenderTextureSensor"/>.
+        /// Note that changing this at runtime does not affect how the Agent sorts the sensors.
+        /// </summary>
+        public string SensorName
+        {
+            get { return m_SensorName; }
+            set { m_SensorName = value; }
+        }
+
+        [HideInInspector, SerializeField, FormerlySerializedAs("grayscale")]
+        bool m_Grayscale;
+
+        /// <summary>
+        /// Whether the RenderTexture observation should be converted to grayscale or not.
+        /// Note that changing this after the sensor is created has no effect.
+        /// </summary>
+        public bool Grayscale
+        {
+            get { return m_Grayscale; }
+            set { m_Grayscale = value; }
+        }
+
+        [HideInInspector, SerializeField]
+        [Range(1, 50)]
+        [Tooltip("Number of frames that will be stacked before being fed to the neural network.")]
+        int m_ObservationStacks = 1;
+
+        [HideInInspector, SerializeField, FormerlySerializedAs("compression")]
+        SensorCompressionType m_Compression = SensorCompressionType.PNG;
+
+        /// <summary>
+        /// Compression type for the render texture observation.
+        /// </summary>
+        public SensorCompressionType CompressionType
+        {
+            get { return m_Compression; }
+            set { m_Compression = value; UpdateSensor(); }
+        }
+        public OptimizedRayCast optimizedRayCast;
+        /// <summary>
+        /// Whether to stack previous observations. Using 1 means no previous observations.
+        /// Note that changing this after the sensor is created has no effect.
+        /// </summary>
+        public int ObservationStacks
+        {
+            get { return m_ObservationStacks; }
+            set { m_ObservationStacks = value; }
+        }
+
+        /// <inheritdoc/>
+        public override ISensor[] CreateSensors()
+        {
+            Dispose();
+            m_Sensor = new RayCNNSensor(optimizedRayCast, texture2D, Grayscale, SensorName, m_Compression);
+            if (ObservationStacks != 1)
+            {
+                return new ISensor[] { new StackingSensor(m_Sensor, ObservationStacks) };
+            }
+            return new ISensor[] { m_Sensor };
+        }
+
+        /// <summary>
+        /// Update fields that are safe to change on the Sensor at runtime.
+        /// </summary>
+        internal void UpdateSensor()
+        {
+            if (m_Sensor != null)
+            {
+                m_Sensor.CompressionType = m_Compression;
+            }
+        }
+
+        /// <summary>
+        /// Clean up the sensor created by CreateSensors().
+        /// Clean up the sensor created by CreateSensors().
+        /// </summary>
+        public void Dispose()
+        {
+            if (!ReferenceEquals(null, m_Sensor))
+            {
+                m_Sensor.Dispose();
+                m_Sensor = null;
+            }
+        }
+    }
+
+
+    public class RayCNNSensor : ISensor, IDisposable
+    {
+        //RenderTexture m_RenderTexture;
+        bool m_Grayscale;
+        string m_Name;
+        private ObservationSpec m_ObservationSpec;
+        SensorCompressionType m_CompressionType;
+        Texture2D m_Texture;
+        OptimizedRayCast optimizedRayCast;
+
+        /// <summary>
+        /// The compression type used by the sensor.
+        /// </summary>
+        public SensorCompressionType CompressionType
+        {
+            get { return m_CompressionType; }
+            set { m_CompressionType = value; }
+        }
+
+
+        /// <summary>
+        /// Initializes the sensor.
+        /// </summary>
+        /// <param name="renderTexture">The [RenderTexture](https://docs.unity3d.com/ScriptReference/RenderTexture.html)
+        /// instance to wrap.</param>
+        /// <param name="grayscale">Whether to convert it to grayscale or not.</param>
+        /// <param name="name">Name of the sensor.</param>
+        /// <param name="compressionType">Compression method for the render texture.</param>
+        /// [GameObject]: https://docs.unity3d.com/Manual/GameObjects.html
+        public RayCNNSensor(OptimizedRayCast s, Texture2D tex,
+            bool grayscale, string name, SensorCompressionType compressionType)
+        {
+            optimizedRayCast = s;
+            var width = tex != null ? tex.width : 0;
+            var height = tex != null ? tex.height : 0;
+            m_Grayscale = grayscale;
+            m_Name = name;
+            m_ObservationSpec = ObservationSpec.Visual(height, width, grayscale ? 1 : 3);
+            m_CompressionType = compressionType;
+            m_Texture = tex;
+            //m_Texture = new Texture2D(width, height, TextureFormat.RGB24, false);
+        }
+
+        /// <inheritdoc/>
+        public string GetName()
+        {
+            return m_Name;
+        }
+
+        /// <inheritdoc/>
+        public ObservationSpec GetObservationSpec()
+        {
+            using (TimerStack.Instance.Scoped("RenderTextureSensor.Write"))
+            {
+                return m_ObservationSpec;
+            }
+
+        }
+
+        /// <inheritdoc/>
+        public byte[] GetCompressedObservation()
+        {
+
+            using (TimerStack.Instance.Scoped("RenderTextureSensor.Write"))
+            {
+                //ObservationToTexture(m_RenderTexture, m_Texture);
+                // TODO support more types here, e.g. JPG
+                var compressed = m_Texture.EncodeToPNG();
+                return compressed;
+            }
+        }
+
+        /// <inheritdoc/>
+        public int Write(ObservationWriter writer)
+        {
+
+
+            var numWritten = writer.WriteTexture(m_Texture, m_Grayscale);
+            optimizedRayCast.ResetPixels();
+            return numWritten;
+
+        }
+
+        /// <inheritdoc/>
+        public void Update() { }
+
+        /// <inheritdoc/>
+        public void Reset() { }
+
+        /// <inheritdoc/>
+        public CompressionSpec GetCompressionSpec()
+        {
+            return new CompressionSpec(m_CompressionType);
+        }
+
+        /// <inheritdoc/>
+        public BuiltInSensorType GetBuiltInSensorType()
+        {
+            return BuiltInSensorType.RenderTextureSensor;
+        }
+
+        /// <summary>
+        /// Converts a RenderTexture to a 2D texture.
+        /// </summary>
+        /// <param name="obsTexture">RenderTexture.</param>
+        /// <param name="texture2D">Texture2D to render to.</param>
+        public static void ObservationToTexture(RenderTexture obsTexture, Texture2D texture2D)
+        {
+            var prevActiveRt = RenderTexture.active;
+            RenderTexture.active = obsTexture;
+
+            texture2D.ReadPixels(new Rect(0, 0, texture2D.width, texture2D.height), 0, 0);
+            texture2D.Apply();
+            RenderTexture.active = prevActiveRt;
+        }
+
+        /// <summary>
+        /// Clean up the owned Texture2D.
+        /// </summary>
+        public void Dispose()
+        {
+            if (!ReferenceEquals(null, m_Texture))
+            {
+
+                m_Texture = null;
+            }
+        }
+    }
+}
