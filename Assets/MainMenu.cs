62f40e325fc3645de01d1bad9b128fbdbfd4feed
Anil Celik Maral <amaral@ucsc.edu>
2023-06-12 18:39:51 +0200

    Added pause menu to Erick's level.

---
+++ Assets/MainMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void OnClickPlayButton() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+
+    public void OnClickQuitButton()
+    {
+        Debug.Log("Quitting the game...");
+        #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        #else
+        Application.Quit();
+        #endif
+    }
+}
