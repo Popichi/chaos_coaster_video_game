33eb816c3a2d8244277d75c91361fa56a7084ea9
Anil Celik Maral <amaral@ucsc.edu>
2023-06-13 00:50:03 +0200

    Added the first version of the customazible wave spawner. There are some bugs. So, it is currently disabled.

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -8,9 +8,12 @@ public class Enemy : MonoBehaviour
   public int maxHealth = 100;
   public int currentHealth;
 
+  private WaveSpawner WaveSpawner;
+
   // Start is called before the first frame update
   void Start() {
     currentHealth = maxHealth;
+    WaveSpawner = GetComponentInParent<WaveSpawner>();
   }
 
   // Update is called once per frame
@@ -22,6 +25,7 @@ public class Enemy : MonoBehaviour
     currentHealth -= amount;
     if (currentHealth <= 0) {
       Destroy(gameObject);
+      --WaveSpawner.waves[WaveSpawner.CurrentWaveIndex].EnemiesLeft;
     }
   }
 }
---
+++ Assets/Scripts/Enemies/WaveSpawner.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveSpawner : MonoBehaviour
+{
+    [SerializeField] private float countdown;
+
+    [SerializeField] private GameObject SpawnPoint;
+
+    public Wave[] waves;
+
+    public int CurrentWaveIndex = 0;
+
+    private bool ReadyToCountDown;
+
+    private void Start() {
+
+        ReadyToCountDown = true;
+
+        for (int i = 0; i < waves.Length; ++i) {
+            waves[i].EnemiesLeft = waves[i].enemies.Length;
+        }
+    }
+
+    private void Update() {
+
+        if (CurrentWaveIndex >= waves.Length) {
+            Debug.Log("You survived every wave!");
+            return;
+        }
+
+        if (ReadyToCountDown) {
+            countdown -= Time.deltaTime;
+        }
+
+        if (countdown <= 0) {
+            ReadyToCountDown = false;
+            countdown = waves[CurrentWaveIndex].TimeToNextWave;
+            StartCoroutine(SpawnWave());
+        }
+
+        if (waves[CurrentWaveIndex].EnemiesLeft == 0) {
+            ReadyToCountDown = true;
+            ++CurrentWaveIndex;
+        }
+    }
+
+    private IEnumerator SpawnWave() {
+            if (CurrentWaveIndex < waves.Length) {
+                for (int i = 0; i < waves[CurrentWaveIndex].enemies.Length; ++i) {
+                Enemy enemy = Instantiate(waves[CurrentWaveIndex].enemies[i], SpawnPoint.transform);
+                enemy.transform.SetParent(SpawnPoint.transform);
+                yield return new WaitForSeconds(waves[CurrentWaveIndex].TimeToNextEnemy);
+            }
+        }
+    }
+}
+
+[System.Serializable]
+public class Wave {
+    public Enemy[] enemies;
+    public float TimeToNextEnemy;
+    public float TimeToNextWave;
+
+    [HideInInspector] public int EnemiesLeft;
+}
