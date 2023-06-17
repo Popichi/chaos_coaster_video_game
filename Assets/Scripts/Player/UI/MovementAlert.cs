ce2481978e2ce33df09679276f32ff38bd1e04b7
Lorenzo Russo da Costa Auer <lorenzo.russo-da-costa-auer@tum.de>
2023-06-17 14:44:47 +0200

    added warning sound to movementalert

--- Assets/Scripts/Player/UI/MovementAlert.cs
+++ Assets/Scripts/Player/UI/MovementAlert.cs
@@ -2,36 +2,41 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using UnityEngine;
-using static System.Net.Mime.MediaTypeNames;
 
 public class MovementAlert : MonoBehaviour
 {
     public Sprite roadSign;
-    public float alertTime = 2f;
+    public float alertTime = 4f;
     public GameObject spritePlaceholder;
     private UnityEngine.UI.Image spritePlaceholderImage;
+    private UnityEngine.AudioSource spritePlaceholderSound;
 
     // Start is called before the first frame update
     void Start()
     {
         Resources.Load(roadSign.name);
-        spritePlaceholder = GameObject.Find("AlertSpritePlaceholder");
+        if (spritePlaceholder == null)
+        {
+            spritePlaceholder = GameObject.Find("AlertSpritePlaceholder");
+        }
         spritePlaceholderImage = spritePlaceholder.GetComponent<UnityEngine.UI.Image>();
+        spritePlaceholderSound = spritePlaceholder.GetComponent<UnityEngine.AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        if(other.gameObject.name == "Follower")
+        if (other.gameObject.name == "Follower")
         {
             spritePlaceholderImage.sprite = roadSign;
             spritePlaceholderImage.enabled = true;
             StartCoroutine(WaitAndDisableAlert());
+            spritePlaceholderSound.Play();
         }
     }
 
