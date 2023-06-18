f3b7259ad09968bd40377c4c507f966bb125c448
Lorenzo Russo da Costa Auer <lorenzo.russo-da-costa-auer@tum.de>
2023-06-18 20:54:22 +0200

    added pause menu to tutorial and added tutorial butotn to main menu

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -9,6 +9,11 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
 
+    public void OnClickTutorialButton()
+    {
+        SceneManager.LoadScene("TutorialAlpha");
+    }
+
 
     public void OnClickQuitButton()
     {
